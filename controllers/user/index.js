b7d9e7372888aca2f8dfcf1db9b0cd46076cbf0a
Nat Poulson <97604337+natpoulson@users.noreply.github.com>
2024-03-14 04:35:30 +1100

    Implement sessions code and GET user route

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -1,40 +1,120 @@
 const route = require('express').Router();
+const { compare } = require('bcrypt');
 const User = require('../../models/user');
 
 const msg = {
     INTERNAL: (operation) => {return `An unexpected error occured when attempting to ${operation}`},
     BAD_REQUEST: (reason) => {return `Bad Request: ${reason}` },
     VALIDATION_FAIL: "One or more values provided failed validation",
-    OPERATION_SUCCESS: (operation) => {return `${operation} succeeded`}
+    OPERATION_SUCCESS: (operation) => {return `${operation} succeeded`},
+    NO_EMAIL: "E-mail address not provided",
+    NO_PASSWORD: "Password not provided",
+    MISSING_PARAMS: "One or more required parameters are missing",
+    NOT_FOUND: "User not found"
 }
 
 const errType = {
-    VALIDATION_FAIL: "SequelizeValidationError"
+    VALIDATION_FAIL: "SequelizeValidationError",
+    MISSING_REQUEST_FIELD: "MissingRequestField"
 }
 
 function errorHandler(error, operation) {
+    const getReasons = () => {
+        return error.errors.map(err => err.message)
+    }
+
     switch (error.name) {
         case errType.VALIDATION_FAIL:
             res.status(400).json({
                 message: msg.VALIDATION_FAIL,
-                reasons: error.errors.map(err => err.message)
+                reasons: getReasons()
             });
             return;
+        case errType.MISSING_REQUEST_FIELD:
+            res.status(400).json({
+                message: msg.BAD_REQUEST(msg.MISSING_PARAMS),
+                reasons: getReasons()
+            })
         default:
             console.error(error);
             res.status(500).json({
-                message: msg.INTERNAL(operation) 
+                message: msg.INTERNAL(operation),
+                reasons: getReasons()
             });
             return;
     }
 }
+
+route.get('/', async (req, res) => {
+    try {
+        // For pulling a user's data when ready
+        const fetchUser = () => {
+            const user = await (User.findByPk(req.session.uid)).get({ plain: true });
+
+            const responseObject = Object.keys(user).forEach(name => {
+                if (name !== 'password') {
+                    responseObject[name] = user[name];
+                }
+            });
+
+            return responseObject;
+        }
+
+        // Just return the user's data if they have an active session
+        if (req.session.signedIn) {
+            res.status(200).json(fetchUser());
+        }
+
+        // Filter out user if there's no email
+        if (!req.body.email) {
+            const err = new Error(msg.NO_EMAIL);
+            err.name = errType.MISSING_REQUEST_FIELD;
+            throw err;
+        }
+
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        // Filter out if user doesn't exist
+        if (!user) {
+            res.status(404).json({
+                message: msg.NOT_FOUND
+            });
+            return;
+        }
+
+        // Filter out if the body has no password
+        if (!req.body.password) {
+            const err = new Error(msg.NO_PASSWORD);
+            err.name = errType.MISSING_REQUEST_FIELD;
+            throw err;
+        }
+
+        // Return the user's data and register a new session if they've provided valid data
+        if (user.checkLogin(req.body.password)) {
+            const user = fetchUser();
+            req.session.save(() => {
+                req.session.signedIn = true;
+                req.session.uid = user.id;
+            });
+            res.status(200).json(user);
+        }
+
+    } catch (error) {
+        errorHandler(error, "fetch a user");
+        return;
+    }
+});
 
 route.post('/', async (req, res) => {
     try {
         // Reject the request if one of the required fields are missing
         if (!req.body.email || !req.body.password) {
             res.status(403).json({
-                message: msg.BAD_REQUEST("one or more required parameters are missing")
+                message: msg.BAD_REQUEST(msg.MISSING_PARAMS)
             });
             return;
         }
@@ -42,6 +122,13 @@ route.post('/', async (req, res) => {
         // Perform operation and normalise the output
         const newUser = (await User.create(req.body)).get({plain: true});
 
+        // Initialise a new session for our user
+        req.session.save(() => {
+            req.session.uid = newUser.id;
+            req.session.signedIn = true;
+        });
+
+        // Return new user record
         res.status(200).json({
             message: msg.OPERATION_SUCCESS("Create"),
             details: {
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
+// Import expresss, database connection, and controllers
 const express = require('express');
 const db = require('./config/db');
 const controller = require('./controllers');
+require('dotenv').config();
 
+// Import and prepare Sessions
+const sessions = require('express-session');
+const SequelizeStore = require('express-session-sequelize')(sessions.Store);
+
+// Define Session parameters
+const session = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new SequelizeStore({
+        db: db
+    })
+}
+
+// Initialise the app
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
+// Initialise middleware
+app.use(sessions(session));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(controller);
 
+// Debug Route
+app.get('/', (req, res) => {
+    res.json({
+        signedIn: req.session.signedIn
+    });
+})
+
+// DB Sync and listener start
 db.sync({alter: true}).then(() => {
     app.listen(PORT, () => {
         console.log("Now listening on port", PORT);
